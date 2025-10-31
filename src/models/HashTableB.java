48797ebf4138823bb7ab7b4d98a75a9db6082425
hi-nataxa <nathasha.0407@gmail.com>
2025-10-31 15:32:27 -0300

    hashtable B

--- src/models/HashTableB.java
+++ src/models/HashTableB.java
@@ -1,5 +1,15 @@
 package models;
 
-public class HashTableB {
+public class HashTableB extends HashTable {
+        @Override
+        public int hash(String key) {
+            int hash = 0;
+            int p = 31;
+            int m = capacity;
 
-}
+            for (int i = 0; i < key.length(); i++) {
+                hash = (hash * p + key.charAt(i)) % m;
+            }
+            return hash;
+        }
+    }
